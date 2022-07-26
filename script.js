31a08c74c0eeaf2af5b99f076cd95f7a35ce5d99
Wesley Militão <wesmilitao@gmail.com>
2022-07-26 10:30:34 -0300

    Commit inicial

---
+++ script.js
@@ -0,0 +1,11 @@
+let cor = 'amarelo' // vermelho,verde,amarelo //
+
+if (cor.toLowerCase() == 'vermelho'){
+    document.getElementById('vermelho').style.backgroundColor = '#D9304F'
+} else if (cor.toLowerCase() == 'amarelo'){
+    document.getElementById('amarelo').style.backgroundColor = '#F2B84B'
+
+} else if (cor.toLowerCase() == 'verde') {
+    document.getElementById('verde').style.backgroundColor = '#4CA649'
+
+} 
